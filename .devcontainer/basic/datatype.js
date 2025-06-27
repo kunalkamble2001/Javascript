e3ef385ad3c29a88a5019829427180ac4e842ead
Kunal Kamble <kunalkamble1729@gmail.com>
2025-06-27 12:07:35 +0000

    Datatypes

---
+++ .devcontainer/basic/datatype.js
@@ -0,0 +1,21 @@
+// this command treat all js code as newer version
+"use strict"; 
+// alert (3+3) // we are using nodejs, not browser
+console.log(3+3)
+console.log("Kunal Kamble");
+ let age = 23
+ let name = ("Kuna")
+ let isLoggedIn = false
+ console.log(typeof age)
+ console.log(name)
+
+// code readability should be high
+// Datatypes =
+// number => 2 to power 53
+// bigint = specially for big numbers like in the trading 
+// string => ""
+// boolean => true/false, right/wrong
+// null =>standalone value
+// undefined => value is not assigned yet
+// symbol => to find the uniqueness
+// object
